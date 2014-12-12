fc6dbf2ec2aad8d4ad7999279c9e55208f9802c6
Nicholas Brown <nichwn@gmail.com>
2014-12-12 21:43:49 +1100

    Skeleton for a console player interface.

---
+++ liars_dice/client/human/console_interface.py
@@ -0,0 +1,66 @@
+"""
+
+An interface for a human player to play the game.
+
+"""
+from twisted.internet.protocol import ClientFactory
+from liars_dice.client.player import Player
+
+
+class ConsoleHuman(Player):
+    """A player instance."""
+
+    def notification_player_status(self, player_data):
+        pass
+
+    def notification_name_request(self):
+        raise NotImplementedError
+
+    def notification_play_request(self):
+        raise NotImplementedError
+
+    def notification_next_turn(self, player):
+        pass
+
+    def notification_hand(self):
+        pass
+
+    def notification_bet(self, face, number):
+        pass
+
+    def notification_spot_on(self):
+        pass
+
+    def notification_liar(self):
+        pass
+
+    def notification_player_lost_die(self, string):
+        pass
+
+    def notification_player_left(self, player):
+        pass
+
+    def notification_player_joined(self, player):
+        pass
+
+    def notification_new_round(self):
+        pass
+
+    def notification_winner(self, player):
+        pass
+
+
+class ConsoleHumanFactory(ClientFactory):
+    """Handle client connections and store the game status."""
+
+    def startedConnecting(self, connector):
+        pass
+
+    def buildProtocol(self, addr):
+        pass
+
+    def clientConnectionLost(self, connector, reason):
+        pass
+
+    def clientConnectionFailed(self, connector, reason):
+        pass
